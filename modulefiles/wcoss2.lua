9be81c6f3745c18cd292dbec05c4fbea95405145
BijuThomas-NOAA <biju.thomas@noaa.gov>
2024-02-02 20:33:24 +0000

    Updating modules based on official installation and consistent with ufs-weather-model

--- modulefiles/wcoss2.lua
+++ modulefiles/wcoss2.lua
@@ -5,23 +5,20 @@ loads hafs_tracker modulefile and related set environment veriables on Cactus an
 envvar_ver=os.getenv("envvar_ver") or "1.0"
 load(pathJoin("envvar", envvar_ver))
 
-PrgEnv_intel_ver=os.getenv("PrgEnv_intel_ver") or "8.1.0"
+PrgEnv_intel_ver=os.getenv("PrgEnv_intel_ver") or "8.3.3"
 load(pathJoin("PrgEnv-intel", PrgEnv_intel_ver))
 
 intel_ver=os.getenv("intel_ver") or "19.1.3.304"
 load(pathJoin("intel", intel_ver))
 
-craype_ver=os.getenv("craype_ver") or "2.7.13"
+craype_ver=os.getenv("craype_ver") or "2.7.17"
 load(pathJoin("craype", craype_ver))
 
-cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.7"
+cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.19"
 load(pathJoin("cray-mpich", cray_mpich_ver))
 
 cray_pals_ver=os.getenv("cray_pals_ver") or "1.2.2"
 load(pathJoin("cray-pals", cray_pals_ver))
-
-prepend_path("MODULEPATH", "/apps/test/hpc-stack/i-19.1.3.304__m-8.1.12__h-1.14.0__n-4.9.2__p-2.5.10__e-8.4.2/modulefiles/compiler/intel/19.1.3.304")
-prepend_path("MODULEPATH", "/apps/test/hpc-stack/i-19.1.3.304__m-8.1.12__h-1.14.0__n-4.9.2__p-2.5.10__e-8.4.2/modulefiles/mpi/intel/19.1.3.304/cray-mpich/8.1.12")
 
 jasper_ver=os.getenv("jasper_ver") or "2.0.25"
 load(pathJoin("jasper", jasper_ver))
@@ -37,15 +34,15 @@ load(pathJoin("libjpeg", libjpeg_ver))
 setenv("JPEG_LIBRARIES", "/apps/spack/libjpeg/9c/intel/19.1.3.304/jkr3isi257ktoouprwaxcn4twtye747z/lib")
 
 hdf5_ver=os.getenv("hdf5_ver") or "1.14.0"
-load(pathJoin("hdf5", hdf5_ver))
+load(pathJoin("hdf5-B", hdf5_ver))
 
 netcdf_ver=os.getenv("netcdf_ver") or "4.9.2"
-load(pathJoin("netcdf", netcdf_ver))
+load(pathJoin("netcdf-B", netcdf_ver))
 
 g2_ver=os.getenv("g2_ver") or "3.4.5"
 load(pathJoin("g2", g2_ver))
 
-w3emc_ver=os.getenv("w3emc_ver") or "2.9.1"
+w3emc_ver=os.getenv("w3emc_ver") or "2.9.2"
 load(pathJoin("w3emc", w3emc_ver))
 
 bacio_ver=os.getenv("bacio_ver") or "2.4.1"
