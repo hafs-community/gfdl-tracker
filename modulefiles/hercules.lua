f88b2778050312e85866a21189c31a58b8285823
Biju Thomas <biju.thomas@noaa.gov>
2024-02-04 15:25:44 -0600

    Update stack-1.6.0 on Hercules

--- modulefiles/hercules.lua
+++ modulefiles/hercules.lua
@@ -5,7 +5,7 @@ loads hafs_tracker modulefile and related set environment veriables on Hercules
 load("contrib")
 load("noaatools") 
 
-prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.5.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
 
 stack_intel_ver=os.getenv("stack_intel_ver") or "2021.9.0"
 load(pathJoin("stack-intel", stack_intel_ver))
@@ -31,7 +31,7 @@ load(pathJoin("hdf5", hdf5_ver))
 netcdf_c_ver=os.getenv("netcdf_c_ver") or "4.9.2"
 load(pathJoin("netcdf-c", netcdf_c_ver))
 
-netcdf_fortran_ver=os.getenv("netcdf_fortran_ver") or "4.6.0"
+netcdf_fortran_ver=os.getenv("netcdf_fortran_ver") or "4.6.1"
 load(pathJoin("netcdf-fortran", netcdf_fortran_ver))
 
 bacio_ver=os.getenv("bacio_ver") or "2.4.1"
