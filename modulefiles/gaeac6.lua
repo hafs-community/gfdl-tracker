05c70493786f8a20f7666fcc38757cc83b04a082
Biju Thomas <biju.thomas@noaa.gov>
2025-08-28 12:07:50 -0400

    Updating modulefiles(wgrib2 with support for ipolates and netcdf) on Gaea C6

--- modulefiles/gaeac6.lua
+++ modulefiles/gaeac6.lua
@@ -2,13 +2,12 @@ help([[
 Load environment to compile TRACKER on Gaea C6 using Intel
 ]])
 
-prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/spack-stack-1.9.2/envs/ue-intel-2023.2.0/install/modulefiles/Core")
-prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/modulefiles")
+prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
 
-stack_intel_ver=os.getenv("stack_intel_ver") or "2023.2.0"
-load(pathJoin("stack-intel", stack_intel_ver))
+stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
+load(pathJoin("stack-oneapi", stack_oneapi_ver))
 
-stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.30"
+stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.32"
 load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
 
 cmake_ver=os.getenv("cmake_ver") or "3.27.9"
@@ -16,7 +15,7 @@ load(pathJoin("cmake", cmake_ver))
 
 load("tracker_common")
 
-unload("cray-libsci")
+--unload("cray-libsci")
 
 setenv("CC", "cc")
 setenv("CXX", "CC")
